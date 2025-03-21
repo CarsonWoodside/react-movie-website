a469798b8c1f3949ab830142d3747f5a97dd0910
CarsonWoodside <carson.woodside@gmail.com>
2025-03-21 01:57:25 +0000

    css added

---
+++ src/pages/Favourites.jsx
@@ -0,0 +1,12 @@
+import "../css/Favourites.css";
+
+function Favourites() {
+  return (
+    <div className="favourites-empty">
+      <h2>No fav movies yet</h2>
+      <p>start adding movies to your favs and they will appear here.</p>
+    </div>
+  );
+}
+
+export default Favourites;
